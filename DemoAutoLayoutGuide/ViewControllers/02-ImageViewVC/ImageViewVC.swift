2489f79d5061085a2f896df3508fc25b881dd87e
Tam Nguyen M <nmint8m@gmail.com>
2019-03-20 23:24:56 +0700

    Demo image view and text view

---
+++ DemoAutoLayoutGuide/ViewControllers/02-ImageViewVC/ImageViewVC.swift
@@ -0,0 +1,18 @@
+//
+//  ImageViewVC.swift
+//  DemoAutoLayoutGuide
+//
+//  Created by Tam Nguyen M. on 3/20/19.
+//  Copyright © 2019 Tam Nguyen M. All rights reserved.
+//
+
+import UIKit
+
+final class ImageViewVC: UIViewController {
+
+    /*
+     This class is for comparing the intrinsic size of image view
+     before and after being assigned property image
+     */
+
+}
