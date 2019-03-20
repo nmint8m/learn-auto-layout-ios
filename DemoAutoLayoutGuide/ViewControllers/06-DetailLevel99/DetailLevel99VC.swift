312c1700021b5ffc54959cec3350678e477d0572
Tam Nguyen M <nmint8m@gmail.com>
2019-03-20 23:34:31 +0700

    Demo detail level 99 ultimate

---
+++ DemoAutoLayoutGuide/ViewControllers/06-DetailLevel99/DetailLevel99VC.swift
@@ -0,0 +1,43 @@
+//
+//  DetailLevel99VC.swift
+//  DemoAutoLayoutGuide
+//
+//  Created by Tam Nguyen M. on 3/20/19.
+//  Copyright © 2019 Tam Nguyen M. All rights reserved.
+//
+
+import UIKit
+
+final class DetailLevel99VC: UIViewController {
+
+    @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var usernameLabel: UILabel!
+    @IBOutlet private weak var starLabel: UILabel!
+    @IBOutlet private weak var trophyLabel: UILabel!
+    @IBOutlet private weak var ultimateButton: UIButton!
+
+    private var isUltimated = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configRoundView(containerView)
+        configRoundView(ultimateButton)
+    }
+
+    private func configRoundView(_ view: UIView) {
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 5
+    }
+
+    @IBAction func ultimateButtonTouchUpInside(_ sender: Any) {
+        isUltimated = !isUltimated
+        usernameLabel.text = isUltimated ? "nmint88888888m" : "nmint8m"
+        starLabel.text = isUltimated ? "⭐️ 999,999" : "⭐️ 99"
+        trophyLabel.text = isUltimated ? "🥇 999,999" : "🥇 99"
+    }
+
+    @IBAction func closeButtonTouchUpInside(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
