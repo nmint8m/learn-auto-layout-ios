af665a3bfbd1898a455e3c9a0519ef8f03ded51d
Tam Nguyen M <nmint8m@gmail.com>
2019-03-22 11:46:16 +0700

    Update project

---
+++ DemoAutoLayoutGuide/ViewControllers/02-Controls/ViewVC.swift
@@ -0,0 +1,16 @@
+//
+//  ViewVC.swift
+//  DemoAutoLayoutGuide
+//
+//  Created by Tam Nguyen M. on 3/22/19.
+//  Copyright © 2019 Tam Nguyen M. All rights reserved.
+//
+
+import UIKit
+
+final class ViewVC: UIViewController {
+
+    /*
+     This class is the example of creating nonambiguous and satifiable layout
+     */
+}
